6178bae2eab9e9a1fcaf792d3aa5c859880024b8
AARAV GUPTA <btech11087.24@bitmesra.ac.in>
2026-06-02 13:50:48 +0530

    Update and rename WINDOWS_FUNCTIONST_AND_RANKINGS.sql to WINDOWS_FUNCTIONS_AND_RANKINGS.sql

--- SQL_Scripts/WINDOWS_FUNCTIONS_AND_RANKINGS.sql
+++ SQL_Scripts/WINDOWS_FUNCTIONS_AND_RANKINGS.sql
@@ -20,3 +20,15 @@ ORDER BY aggregate_rating DESC
 ) AS row_num
 
 FROM zomato_clean;
+
+-- RESTAURANT RANKING WITHIN EACH CITY --
+SELECT
+restaurant_name,
+city,
+aggregate_rating,
+votes,
+RANK() OVER(
+PARTITION BY city
+ORDER BY aggregate_rating DESC
+) AS city_rank
+FROM zomato_clean;
