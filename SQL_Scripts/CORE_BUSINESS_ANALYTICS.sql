bdf5efe7ef20a7937f3d02e1af2ca0fec7324381
AARAV GUPTA <btech11087.24@bitmesra.ac.in>
2026-06-02 13:46:19 +0530

    Update CORE_BUSINESS_ANALYTICS.sql

--- SQL_Scripts/CORE_BUSINESS_ANALYTICS.sql
+++ SQL_Scripts/CORE_BUSINESS_ANALYTICS.sql
@@ -17,3 +17,17 @@ WHERE aggregate_rating > 0
 GROUP BY cuisines
 HAVING COUNT(*) >= 10 -- removes cuisines with very few restaurants --
 ORDER BY avg_rating DESC;
+
+-- PRICE RANGE ANALYSIS --
+-- 4: LUXURY
+-- 3: PREMIUM
+-- 2: MODERATE
+-- 1: BUDGET
+
+SELECT price_range,
+COUNT(*) AS restaurants,
+ROUND(AVG(aggregate_rating),2) AS avg_rating,
+ROUND(AVG(votes),0) AS avg_votes
+FROM zomato_clean
+GROUP BY price_range
+ORDER BY avg_votes DESC;
