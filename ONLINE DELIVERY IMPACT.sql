9ecedcfb43858422fa947f9e923c381795b97e64
AARAV GUPTA <btech11087.24@bitmesra.ac.in>
2026-06-02 13:53:46 +0530

    Delete ONLINE DELIVERY IMPACT.sql

--- ONLINE DELIVERY IMPACT.sql
+++
@@ -1,8 +0,0 @@
--- ONLINE DELIVERY IMPACT --
-
-SELECT has_online_delivery,
-COUNT(*) AS restaurants,
-ROUND(AVG(aggregate_rating),2) AS avg_rating,
-ROUND(AVG(votes),0) AS avg_votes
-FROM zomato_clean
-GROUP BY has_online_delivery;
