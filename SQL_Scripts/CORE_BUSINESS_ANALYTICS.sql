29936bdcbe5ea79bca64edfb265f32d7d91a1646
AARAV GUPTA <btech11087.24@bitmesra.ac.in>
2026-06-02 13:45:41 +0530

    Update CORE_BUSINESS_ANALYTICS.sql

--- SQL_Scripts/CORE_BUSINESS_ANALYTICS.sql
+++ SQL_Scripts/CORE_BUSINESS_ANALYTICS.sql
@@ -6,3 +6,14 @@ ROUND(AVG(aggregate_rating),2) AS avg_rating,
 ROUND(AVG(votes),0) AS avg_votes
 FROM zomato_clean
 GROUP BY has_online_delivery;
+
+-- TOP RATED CUISINES --
+
+SELECT cuisines,
+COUNT(*) AS restaurants,
+ROUND(AVG(aggregate_rating),2) AS avg_rating
+FROM zomato_clean
+WHERE aggregate_rating > 0
+GROUP BY cuisines
+HAVING COUNT(*) >= 10 -- removes cuisines with very few restaurants --
+ORDER BY avg_rating DESC;
